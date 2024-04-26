880af1144076e62cf36fb3da7da1c3d35e20f7cb
Dmytro Artiuk <dimairak88@wp.pl>
2024-04-26 10:22:37 +0200

    added div with className container

--- src/App/Form/index.js
+++ src/App/Form/index.js
@@ -24,44 +24,46 @@ export const Form = () => {
     };
 
     return (
-        <form className="form" onSubmit={onFormSubmit}>
-            <fieldset className="form__fieldset">
-                <legend className="form__legend">Kantor złotóweczka</legend>
-                <p className="form__paragraph">
-                    <label className="form__label">
-                        <span className="form__labelText">Tyle mam PLN*:</span>
-                        <input value={amount} onChange={({ target }) => setAmount(target.value)} type="number" required step="1" className="form__field" placeholder="wpisz kwotę" name="enteredAmount"
-                        />
-                    </label>
-                </p>
-                <p>
-                    <label>
-                        <span className="form__labelText">
-                            Wybierz waluta:
-                        </span>
-                        <select value={currency} onChange={({ target }) => setCurrency(target.value)} className="form__field" required name="selectedCurrency">
-                            {currencies.map((currency => (
-                                <option
-                                    key={currency.short}
-                                    value={currency.short}
-                                >
-                                    {currency.name}
-                                </option>
-                            )))}
-                        </select>
-                    </label>
-                </p>
+        <div className="container">
+            <form className="form" onSubmit={onFormSubmit}>
+                <fieldset className="form__fieldset">
+                    <legend className="form__legend">Kantor złotóweczka</legend>
+                    <p className="form__paragraph">
+                        <label className="form__label">
+                            <span className="form__labelText">Tyle mam PLN*:</span>
+                            <input value={amount} onChange={({ target }) => setAmount(target.value)} type="number" required step="1" className="form__field" placeholder="wpisz kwotę" name="enteredAmount"
+                            />
+                        </label>
+                    </p>
+                    <p>
+                        <label>
+                            <span className="form__labelText">
+                                Wybierz waluta:
+                            </span>
+                            <select value={currency} onChange={({ target }) => setCurrency(target.value)} className="form__field" required name="selectedCurrency">
+                                {currencies.map((currency => (
+                                    <option
+                                        key={currency.short}
+                                        value={currency.short}
+                                    >
+                                        {currency.name}
+                                    </option>
+                                )))}
+                            </select>
+                        </label>
+                    </p>
 
-                <Result result={result} />
+                    <Result result={result} />
 
-                <p>
-                    <button className="form__button">Przelicz dla mnie</button>
-                    <button type="reset" className="form__button--special">wyczyść kwotę</button>
-                </p>
-                <p className="form__footer">
-                    *Pole obowiązkowe do wypełnienia
-                </p>
-            </fieldset>
-        </form>
+                    <p>
+                        <button className="form__button">Przelicz dla mnie</button>
+                        <button type="reset" className="form__button--special">wyczyść kwotę</button>
+                    </p>
+                    <p className="form__footer">
+                        *Pole obowiązkowe do wypełnienia
+                    </p>
+                </fieldset>
+            </form>
+        </div>
     );
 };
