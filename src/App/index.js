fe8a8baeb7c8733aba453f05c72ebe4338c7b87d
Dmytro Artiuk <dimairak88@wp.pl>
2024-05-21 13:19:55 +0200

    add clock

--- src/App/index.js
+++ src/App/index.js
@@ -1,9 +1,11 @@
 import { Form } from "./Form";
 import "./app.css";
+import { Clock } from "./Clock";
 
 function App() {
     return (
         <div className="app, container">
+            <Clock />
             <Form />
         </div>
     );
