45a6def8e5e239e2d9cd394ecc4336facc3c1750
Dmytro Artiuk <dimairak88@wp.pl>
2024-06-22 09:32:39 +0200

    added folder theme.js

---
+++ src/theme.js
@@ -0,0 +1,18 @@
+export const theme = {
+    colors: {
+        veryLightGray: "#ccc",
+        blueRbbon: "#033ff9",
+        darkCyan: "rgb(o, 139, 139)",
+        blueViolet: "rgb(138, 43, 226)",
+        jade: "rgb(1, 185, 124)",
+        crimson: "rgb(220, 20, 60)",
+        desertStorm: "hsl(0, 79%, 87%)",
+        fadedJade: "rgb(42, 106,106)",
+        gothic: "hsl(182, 25%, 45%)",
+        mauve: "hsl(271, 76%, 79%)",
+    },
+
+    breakpoint: {
+        mobileMax: 767,
+    },
+}
