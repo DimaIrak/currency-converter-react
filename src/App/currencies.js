51fac9bc73ccaaf48a4a6253fd9f73dedb5155a1
Dmytro Artiuk <dimairak88@wp.pl>
2024-07-03 22:58:52 +0200

    currency hryvnia was added to the table by currencies

--- src/App/currencies.js
+++ src/App/currencies.js
@@ -2,16 +2,21 @@ export const currencies = [
     {
         short: "USD",
         name: "Dolar amerykański",
-        rate: 4.4278,
+        rate: 4.0,
     },
     {
         short: "EUR",
         name: "Euro",
-        rate: 4.9384,
+        rate: 4.31,
     },
     {
         short: "GBP",
         name: "Funt brytyjski",
-        rate: 3.9058,
+        rate: 5.08,
+    },
+    {
+        short: "UAH",
+        name: "Hrywna",
+        rate: 0.1,
     },
 ];
