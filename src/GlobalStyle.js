107de4db4ba0771574fc073fd6fc1a845227ef5f
Dmytro Artiuk <dimairak88@wp.pl>
2024-07-04 20:24:51 +0200

    added indentations in GlobalStyle.js

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -3,21 +3,20 @@ import background from "./images/background.jpg"
 
 export const GlobalStyle = createGlobalStyle`
     html {
-    box-sizing: border-box;
+        box-sizing: border-box;
     }
 
     *, ::after, ::before {
-    box-sizing: inherit;
+        box-sizing: inherit;
     }
 
     #root {
-    min-height: 100vh;
-    font-family: 'Lato', sans-serif;
-    background: url("${background}") center no-repeat;
-    background-position: center;
-    background-size: cover;
-    background-attachment: fixed;
-    
+        min-height: 100vh;
+        font-family: 'Lato', sans-serif;
+        background: url("${background}") center no-repeat;
+        background-position: center;
+        background-size: cover;
+        background-attachment: fixed;
     }
 
     button {
