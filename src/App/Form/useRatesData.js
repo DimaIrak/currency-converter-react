47a05a96a3bc4936fa42aed3c7b23dad34cae43d
Dmytro Artiuk <dimairak88@wp.pl>
2024-07-20 11:29:06 +0200

    added the URL to the API as a constant in useRatesData.js

--- src/App/Form/useRatesData.js
+++ src/App/Form/useRatesData.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 
 export const useRatesData = () => {
+    const API_URL = "https://api.currencyapi.com/v3/latest?apikey=cur_live_dicjdNB7adLyo9HvvTsJla2S6wny6LS6NBRbBpKM&base_currency=PLN";
     const [ratesData, setRatesData] = useState({
         state: "loading",
     });
@@ -8,8 +9,7 @@ export const useRatesData = () => {
     useEffect(() => {
         const fetchRates = async () => {
             try {
-                const response = await fetch("https://api.currencyapi.com/v3/latest?apikey=cur_live_dicjdNB7adLyo9HvvTsJla2S6wny6LS6NBRbBpKM&base_currency=PLN")
-
+                const response = await fetch(API_URL);
                 if (!response.ok) {
                     throw new Error(response.statusText);
                 }
